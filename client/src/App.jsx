10f1a9c1bdbb826e5c5c76982b81e3ca8a9901a6
siyangllA <siyanglabhote@gmail.com>
2025-04-21 15:38:44 +0545

    create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './Pages/CreateListing';
 import UpdateListing from './Pages/UpdateListing';
 import Listing from './Pages/Listing';
+import Search from './Pages/Search';
+
 
 
 export default function App() {
@@ -20,6 +22,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
         <Route element={<PrivateRoute />} >
           <Route path="/profile" element={<Profile />} />
