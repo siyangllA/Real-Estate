d5fb0af572d4364c48c6faa4fa4b802a61ae4a9b
siyangllA <siyanglabhote@gmail.com>
2025-04-21 20:38:34 +0545

    listing update

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -8,7 +8,7 @@ export default function ListingItem({ listing }) {
         <img
           src={
             listing.imageUrls[0] ||
-            'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
+            'https://english.onlinekhabar.com/wp-content/uploads/2024/01/real-estate-house.jpg'
           }
           alt='listing cover'
           className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
