84c1af93920c32f951c97a5e9472ed6b54d3a995
siyangllA <siyanglabhote@gmail.com>
2025-04-21 23:35:40 +0545

    fixing again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp'; 
+import SignUp from './Pages/SignUp';
 import Profile from './Pages/Profile';
-import About from './Pages/about';
+import About from './Pages/About';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './Pages/CreateListing';
@@ -11,29 +11,25 @@ import UpdateListing from './Pages/UpdateListing';
 import Listing from './Pages/Listing';
 import Search from './Pages/Search';
 
-
-
 export default function App() {
   return (
     <BrowserRouter>
-    <Header></Header>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/listing/:listingId' element={<Listing />} />
-        <Route element={<PrivateRoute />} >
+        <Route path="/search" element={<Search />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+
+        {/* Private Routes */}
+        <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          <Route
-             path='/update-listing/:listingId'
-             element={<UpdateListing />}
-           />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
         </Route>
-        </Routes>
+      </Routes>
     </BrowserRouter>
   );
 }
-
