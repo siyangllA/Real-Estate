ee2ec4672fc688f06156fc9988429018d786dcae
Kritikaadhikarii <itskritikaaa@gmail.com>
2025-07-15 00:05:53 +0545

    remove search from navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -43,23 +43,7 @@ export default function Header() {
         </h1>
         </Link>
 
-      {!hideSearch && (
-        <form onSubmit={handleSubmit} 
-        className='bg-slate-100 p-3 rounded-lg flex items-center'>
-          <input
-            type='text'
-            placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
 
-          />
-          <button>
-            <FaSearch className='text-slate-600' />
-          </button>
-
-        </form>
-      )}
 
 
         <ul className='flex gap-4'>
